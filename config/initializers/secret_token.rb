531a4e79892d3711d49d1420dca4a41fdaaf28d2
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-23 07:33:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NinefoldSandbox::Application.config.secret_key_base = 'f6993a18cdd90ca2da5ce4c44dae6217760b69e86ddf4c6892cd996f4eb594cf867ccadc9aac51c3db48ee669057e6e6dcd4c10b8ea554484ee8f005e1f8b82c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NinefoldSandbox::Application.config.session_store :cookie_store, key: '_ninefold-sandbox_session'
